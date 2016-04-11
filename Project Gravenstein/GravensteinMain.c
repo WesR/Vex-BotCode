2ae787c35b5355ace54d01770698d06249e05896
Wes Ring <wring436@gmail.com>
2016-04-11 16:47:44 -0400

    Documentation and forward time

--- Project Gravenstein/GravensteinMain.c
+++ Project Gravenstein/GravensteinMain.c
@@ -20,23 +20,28 @@ int launcherSpeed = 0;
 int launcherSpeed_new = 0;
 bool hozBelt_on = false;
 
-void setLauncherSpeed(int _speed) {
+void setLauncherSpeed(int _speed){ //Sets the launcher speed
 	setMotor(launcherLeft, -_speed);
 	setMotor(launcherRight, _speed);
 }
-
-void stopLauncher(void){
+void stopLauncher(void){ //Kills the launcher
 	stopMotor(launcherLeft);
 	stopMotor(launcherRight);
 	launcherSpeed = 0;
 	launcherSpeed_new = 0;
 }
-void updateLauncherSpeed(void){
+void updateLauncherSpeed(void){ //Updates the launcher speed changes
 	if (launcherSpeed < launcherSpeed_new){launcherSpeed++;wait(.1);}
 	if (launcherSpeed > launcherSpeed_new){launcherSpeed--;wait(.1);}
 	setLauncherSpeed(launcherSpeed);
 }
-
+void goForwardFor(int time){ //Goes forward for a set ammount of time
+	setMotor(leftMotor,120);
+	setMotor(rightMotor,120);
+	wait(time);
+	stopMotor(leftMotor);
+	stopMotor(rightMotor);
+}
 void roboControl(void){ //For autonomous control	//This is test code
 	//We spin in a circle for 5 seconds
 	setMotor(leftMotor,100);
